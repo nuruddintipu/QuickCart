37507750531d45f24ea57d0409cead0c78068784
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-07-03 21:57:44 +0600

    feat: add basic styling for page to take available space

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./routes/AppRouter.jsx";
 import {ToastContainer} from "react-toastify";
+import './App.css';
 
 function App() {
   return (
     <>
         <Router>
-            <AppRouter />
+            <div className="app-container">
+                <AppRouter />
+            </div>
         </Router>
         <ToastContainer position="bottom-right"  autoClose={2000} />
     </>
