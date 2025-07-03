4831868e3e25ac629fd6cdcd2793830b8e165a19
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-07-03 20:02:37 +0600

    feat: add cartReducer to redux central store

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import productsReducer from "../features/productsSlice.jsx";
+import cartReducer from "../features/cartSlice.jsx";
 
 const store = configureStore({
     reducer: {
         products: productsReducer,
+        cart: cartReducer
     }
 });
 
