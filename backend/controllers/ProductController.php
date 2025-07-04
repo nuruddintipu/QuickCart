c6f29098a4d16482a4a1f12d7e6a9b6d494780dd
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-07-04 12:27:23 +0600

    fix: prepend base URL to product images in API responses

--- backend/controllers/ProductController.php
+++ backend/controllers/ProductController.php
@@ -12,7 +12,11 @@ class ProductController{
     public function getAllProducts(){
         try{
             $products = $this->productService->getAllProducts();
-            if($products) {
+            if ($products) {
+                $baseUrl = $this->getBaseUrl();
+                foreach ($products as &$product) {
+                    $product['image'] = $baseUrl . $product['image'];
+                }
                 Response::dataFoundResponse($products);
             } else {
                 Response::dataNotFoundResponse('No products found');;
@@ -22,10 +26,17 @@ class ProductController{
         }
     }
 
+    private function getBaseUrl(){
+        $scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+        $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+        return $scheme . '://' . $host;
+    }
+
     public function getProductById($id){
         try{
             $product = $this->productService->getProductById($id);
             if($product) {
+                $product['image'] = $this->getBaseUrl() . $product['image'];
                 Response::dataFoundResponse($product);
             } else {
                 Response::dataNotFoundResponse('Product not found');;
