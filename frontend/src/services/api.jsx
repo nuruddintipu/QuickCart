48929a5715bc58f72289cdee954ee841470db150
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-07-04 19:47:54 +0600

    refactor: update API base URL

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8000/';
+const BASE_URL = 'https://api-quickcart.infinityfreeapp.com/';
 
 const apiClient = axios.create({
     baseURL: BASE_URL,
